01a51b214e227d0aedabe4656fc93a8c46cb4e4e
sobralmps <sobralmps@gmail.com>
2020-05-06 18:09:34 -0300

    Indentacao

--- Exercícios/parte1/exercicio2.java
+++ Exercícios/parte1/exercicio2.java
@@ -3,13 +3,13 @@ package parte1;
 import java.io.IOException;
 
 public class exercicio2 {
-	
-    public static void main(String [] args) throws IOException {
-    	
-      System.out.println("Digite uma palavra: ");
-      char c = (char)System.in.read();
-      System.out.printf("%c",c);
-      
-    }
-    
+
+	public static void main(String [] args) throws IOException {
+
+		System.out.println("Digite uma palavra: ");
+		char c = (char)System.in.read();
+		System.out.printf("%c",c);
+
+	}
+
 }
